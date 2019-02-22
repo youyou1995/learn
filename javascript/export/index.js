cf8db860a8fa06c84dc13d08aa6e2a3fc8db4800
sunqy <mrsunqy@163.com>
2019-02-22 18:17:55 +0800

    添加less教程

---
+++ javascript/export/index.js
@@ -0,0 +1,5 @@
+// import index1 from './index1'
+import index2 from './index2'
+import index3 from './index3'
+
+console.log('index',  index2, index3);
---
+++ javascript/export/index1.js
@@ -0,0 +1,6 @@
+const index1 = {
+    a: 1,
+    b: 2
+}
+
+export default index1
